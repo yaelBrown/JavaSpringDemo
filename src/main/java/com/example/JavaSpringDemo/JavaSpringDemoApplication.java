060df3d52199a2e92262d60ddd549d1f48958c25
Yael Brown <yaelrbrown@gmail.com>
2019-05-27 23:52:21 -0400

    update spring demo
    installed java se 8

---
+++ src/main/java/com/example/JavaSpringDemo/JavaSpringDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.JavaSpringDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringDemoApplication.class, args);
+	}
+
+}
